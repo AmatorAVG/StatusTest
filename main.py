04d584fb8ab35361f8f4f68450a00ae607eb6ba0
AmatorAVG <amatoravg@gmail.com>
2022-11-24 11:27:36 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+class TreeStore:
+    def __init__(self, elements):
+        self.branches = {el["id"]: el for el in elements}
+
+    def getAll(self):
+        return [el for el in self.branches.values()]
+
+    def getItem(self, el):
+        return self.branches[el]
+
+    def getChildren(self, parent):
+        return [el for el in self.branches.values() if el["parent"] == parent]
+
+    def getAllParents(self, el):
+        res = []
+        while self.branches[el]["parent"] != "root":
+            el = self.branches[el]["parent"]
+            res.append(self.branches[el])
+        return res
+
+
+items = [
+    {"id": 1, "parent": "root"},
+    {"id": 2, "parent": 1, "type": "test"},
+    {"id": 3, "parent": 1, "type": "test"},
+    {"id": 4, "parent": 2, "type": "test"},
+    {"id": 5, "parent": 2, "type": "test"},
+    {"id": 6, "parent": 2, "type": "test"},
+    {"id": 7, "parent": 4, "type": None},
+    {"id": 8, "parent": 4, "type": None}
+]
+ts = TreeStore(items)
+
+# Примеры использования:
+#  - ts.getAll() // [{"id":1,"parent":"root"},{"id":2,"parent":1,"type":"test"},{"id":3,"parent":1,"type":"test"},{"id":4,"parent":2,"type":"test"},{"id":5,"parent":2,"type":"test"},{"id":6,"parent":2,"type":"test"},{"id":7,"parent":4,"type":None},{"id":8,"parent":4,"type":None}]
+#
+#  - ts.getItem(7) // {"id":7,"parent":4,"type":None}
+#
+#  - ts.getChildren(4) // [{"id":7,"parent":4,"type":None},{"id":8,"parent":4,"type":None}]
+#  - ts.getChildren(5) // []
+#
+#  - ts.getAllParents(7) // [{"id":4,"parent":2,"type":"test"},{"id":2,"parent":1,"type":"test"},{"id":1,"parent":"root"}]
